97282a0201d65319b1a4acd3d2e83f57d69272e1
tmas0 <antoni.mas@gmail.com>
2015-06-09 13:11:49 +0200

    Despliege heroku + PostgreSQL

--- models/models.js
+++ models/models.js
@@ -1,11 +1,28 @@
 var path = require('path')
 
+// PostgreSQL DATABASE_URL = postgres://user@passwd@host:port/database
+// SQLite     DATABASE_URL = sqlite = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
 // Cargar el modelo ORM.
 var Sequelize = require('sequelize');
 
 // Usar BD SQLite.
-var sequelize = new Sequelize(null, null, null,
-                        {dialect: 'sqlite', storage: "quiz.sqlite"}
+var sequelize = new Sequelize(DB_name, user, pwd,
+                        {dialect: dialect,
+                         protocol: protocol,
+                         port: port,
+                         host: host,
+                         storage: storage,
+                         omitNull: true}
                     );
                     
                     
