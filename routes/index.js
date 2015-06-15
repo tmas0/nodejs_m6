579819708d9dea65e072b7f62d5d4a2d748056bf
tmas0 <antoni.mas@gmail.com>
2015-06-15 11:05:25 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId.
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load); // Autoload :commentId
 
 // Definición de rutas de sesión.
 router.get('/login', sessionController.new); // Formulario de login.
@@ -32,6 +33,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 // Comments controller routes.
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 router.get('/author', function(req, res) {
     res.render('author');
